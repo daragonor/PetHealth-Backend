916cc4814989976139cc8d91f33b0a2d0194768f
Daniel <danie.aragon.0235@hotmail.com>
2019-05-17 15:00:52 -0500

    sign up finished

--- src/routes/auth-route.js
+++ src/routes/auth-route.js
@@ -11,12 +11,45 @@ router.post('/signup',async (req,res) => {
         message:"Signed Up succesfuly.",
         data: null
     }
-    const { username, password, mail, photo, userable_type } = req.body;
+    const { username, password, mail, photo, userable_type, 
+        name, last_name, dni, phone,address,
+        linkedin_link, degree,
+        location, opening_hours,
+        website_url, youtube_url, twitter_url } = req.body;
     let user = {username,password,mail,photo,userable_type};
     user.password = await helpers.encryptPassword(password);
     // Saving in the Database
-    const result = await connection.query('INSERT INTO User SET ? ', User);
-    res.json(response)
+    connection.query('INSERT INTO User SET ? ', user, async (err, rows)=>{
+        const userId = rows.insertId
+        let person = {person_id : userId, name, last_name, dni, address, phone}
+        switch (userable_type) {
+            case 0:
+                let socialUrl = {website_url, youtube_url, twitter_url}
+                connection.query('INSERT INTO SocialURL SET ? ', socialUrl, (err, rows) => {
+                    const socialURLId = rows.insertId
+                    let veterinary = {veterinary_id : userId, social_url_id : socialURLId, name, phone, location, opening_hours}
+                    connection.query('INSERT INTO Veterinary SET ? ', veterinary, (err, rows) => {
+                        res.status(200).send(response)
+                    })
+                })
+            case 1:
+                let vet = {veterinarian_id : userId, linkedin_link, degree}
+                connection.query('INSERT INTO Person SET ? ', person, (err, rows) => {
+                    connection.query('INSERT INTO Veterinarian SET ? ', vet, (err, rows) => {
+                        res.status(200).send(response)
+                    })
+                })
+            case 2:
+                let customer = {customer_id : userId}
+                connection.query('INSERT INTO Person SET ? ', person, (err, rows) => {
+                    connection.query('INSERT INTO Customer SET ? ', customer, (err, rows) => {
+                        res.status(200).send(response)
+                    })
+                })
+            default:
+                break;
+        }
+    })
 });
 
 
