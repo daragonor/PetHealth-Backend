adde569d963dc07835fcd8f179de9869cd387d0b
Khanatoshs <leo.hs334@gmail.com>
2019-05-27 10:28:14 -0500

    corregido error de verificacion

--- src/routes/pets-route.js
+++ src/routes/pets-route.js
@@ -6,7 +6,6 @@ const helpers = require('../lib/helpers');
 const petAPI = require('../models/pet.js');
 
 router.get('/user/:userId/pets',helpers.verifyToken,(req,res)=>{
-        if(!err){
             let userId = req.params.userId;
             petAPI.getAll(userId,(pets,err)=>{
                 let response = {
@@ -23,13 +22,9 @@ router.get('/user/:userId/pets',helpers.verifyToken,(req,res)=>{
                 }
                 res.status(200).send(response);
             })
-        }else{
-            res.sendStatus(403);
-        }
 });
 
 router.get('/user/:userId/pets/:id',helpers.verifyToken,(req,res)=>{
-        if(!err){
             let id = req.params.id;
             let userId = req.params.userId;
             petAPI.getPet(userId,id,(pet,err)=>{
@@ -47,13 +42,9 @@ router.get('/user/:userId/pets/:id',helpers.verifyToken,(req,res)=>{
                 }
                 res.status(200).send(response);
             });
-        }else{
-            res.sendStatus(403);
-        }
 });
 
 router.post('/user/:userId/pets',helpers.verifyToken,(req,res)=>{
-        if(!err){
             console.log(req.body);
             let userId = parseInt(req.params.userId);
             const {name, description,race,birth_date,status,image_url} = req.body;
@@ -74,14 +65,9 @@ router.post('/user/:userId/pets',helpers.verifyToken,(req,res)=>{
                 }
                 res.status(200).send(response);
             });
-        }else{
-            res.sendStatus(403);
-        }
-   
 });
 
 router.put('/user/:userId/pets/:id',helpers.verifyToken,(req,res)=>{
-        if(!err){
             let userId = req.params.userId;
             let petId = req.params.id;
             const {name, description,race,birth_date,status,image_url} = req.body;
@@ -102,13 +88,9 @@ router.put('/user/:userId/pets/:id',helpers.verifyToken,(req,res)=>{
                 }
                 res.status(200).send(response);
             });
-        }else{
-            res.sendStatus(403);
-        }
 });
 
 router.delete('/user/:userId/pets/:id',helpers.verifyToken,(req,res)=>{
-        if(!err){
             let userId = req.params.userId;
             let petId = req.params.id;
             petAPI.deletePet(userId,petId,(err)=>{
@@ -126,9 +108,6 @@ router.delete('/user/:userId/pets/:id',helpers.verifyToken,(req,res)=>{
                 }
                 res.status(200).send(response);
             });
-        }else{
-            res.sendStatus(403);
-        }
 });
 
 module.exports = router;
