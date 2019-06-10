13458d452e8f035f7f8fe4dc5b2e42c0da827a42
Khanatoshs <leo.hs334@gmail.com>
2019-06-10 12:18:27 -0500

    Rutas de veterinaria y cambio de latitud y longitug en appointments

--- src/models/veterinary.js
+++ src/models/veterinary.js
@@ -2,25 +2,29 @@ const connection  = require('../database.js');
 const helpers = require('../lib/helpers');
 
 class Veterinary {
-    constructor (id, social_url_id, name, phone, location,opening_hours){
+    constructor (id, social_url_id, name, phone, location,opening_hours,latitude,longitude){
         this.id = id
         this.social_url_id = social_url_id
         this.name = name
         this.phone = phone
         this.location = location
         this.opening_hours = opening_hours
+        this.latitude = latitude
+        this.longitude = longitude
     }
     getVeterinary(id,handler){
       connection.query('SELECT * FROM Veterinary WHERE veterinary_id = ? ', [id], (err, rows) => {
         if(!err) {
-          const veterinary = rows[0]
+          const veterinary = rows[0];
           const response = new Veterinary(
             veterinary.veterinary_id,
             veterinary.social_url_id,
             veterinary.name,
             veterinary.phone,
             veterinary.location,
-            veterinary.opening_hours)
+            veterinary.opening_hours,
+            veterinary.latitude,
+            veterinary.longitude)
           handler(response,null)
         } else {
           console.log(err);
@@ -39,30 +43,52 @@ class Veterinary {
       })
     }
 
-    getCloseVeterinaries(location,radius,handler){
-      let veterinaries = [];
+    getCloseVeterinaries(location,handler){
+      let dataVeterinaries = [];
+      //let veterinaries = [];
       connection.query('SELECT * FROM Veterinary',(err,rows)=>{
         if(err){
           console.log(err);
           handler(null,err);
         }else{
+          const limit = 1;
           rows.forEach(vet => {
+            let dataVet = {
+              veterinary: null,
+              distance: 0
+            }
             let locationVet = {
               lat: vet.latitude,
               long: vet.longitude
             }
-            if(helpers.distance(location,locationVet)<=radius){
-              veterinaries.push(new Veterinary(
-                vet.id,
-                vet.social_url_id,
-                vet.name,
-                vet.phone,
-                vet.location,
-                vet.opening_hours
-              ));
-            }
+            dataVet.veterinary = new Veterinary(
+              vet.id,
+              vet.social_url_id,
+              vet.name,
+              vet.phone,
+              vet.location,
+              vet.opening_hours,
+              vet.latitude,
+              vet.longitude
+            );
+            dataVet.distance = helpers.distance(location,locationVet);
+            dataVeterinaries.push(dataVet);
+            /*veterinaries.push(new Veterinary(
+              vet.id,
+              vet.social_url_id,
+              vet.name,
+              vet.phone,
+              vet.location,
+              vet.opening_hours,
+              vet.latitude,
+              vet.longitude
+            ));*/
+            
+            //dataVeterinary.distance.push(helpers.distance(location,locationVet));
           });
-          handler(veterinaries,null);
+          dataVeterinaries.sort((data1,data2)=>parseFloat(data1.distance)-parseFloat(data2.distance));
+          let resData = dataVeterinaries.slice(0,limit);
+          handler(resData,null);
         }
       });
     }
