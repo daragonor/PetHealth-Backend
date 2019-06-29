8c4ad370c0b522279a1d2262d6c7f68fdff82b02
LeoDev94 <u201510070@upc.edu.pe>
2019-06-29 12:32:49 -0500

    Laniding antigua deployada

--- src/server.js
+++ src/server.js
@@ -5,10 +5,11 @@ const app = express();
 
 // Settings
 app.set('port', process.env.PORT || 3000);
-
+app.set('view engine', 'ejs');
+app.set('views',__dirname+'/views');
 // Middlewares
 app.use(express.json());
-
+app.use(express.static(__dirname+'/public'));
 // Routes
 app.use('/api',require('./routes/home-route'));
 app.use('/api',require('./routes/appt-route'));
@@ -20,6 +21,7 @@ app.use('/api',require('./routes/user-route'));
 app.use('/api',require('./routes/history-route'));
 app.use('/api',require('./routes/customer-route'));
 app.use('/api',require('./routes/veterinarian-route'));
+app.use('/',require('./routes/landing-route'));
 
 
 // Starting the server
